4167d6a91d98321cb29b8f2ff8a7a94bde705227
박성준 <@121.156.237.188>
2021-06-10 00:41:51 +0900

    0609

---
+++ spring/src/main/java/controller/employees/EmployeeController.java
@@ -0,0 +1,27 @@
+package controller.employees;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import command.EmployeeCommand;
+
+@Controller
+@RequestMapping("emp")
+public class EmployeeController {
+	@RequestMapping("empList")
+	public String empList() {
+		return "employee/empList";
+	}
+	@RequestMapping("empReget")
+	public String empReget() {
+		return "employee/employeeForm";
+	}
+	@RequestMapping(value="empJoin", method=RequestMethod.POST)
+	public String  empJoin(EmployeeCommand employeeCommand) {
+		
+		System.out.println(employeeCommand.getEmpId());
+		
+		return "redirect:empList";
+	}
+}
